a192906991e8f5f36e2c954fa436786a62839c67
KatieCrowhurst <katie.louise.crowhurst1995@hotmail.com>
2020-11-15 19:06:13 +0000

    Re-design Portfolio Website
    * Add animated background using Vanta.Js
    * Add navigation icons and styling.
    * Left-align my text content.
    * Add CSS typing animation to my first name.
    * Refactor CSS code.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+// Vanta Background 
+
+const backgroundEffect = VANTA.NET({
+    el: "#vanta-background",
+    mouseControls: true,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: '#6831c0',
+    backgroundColor: '#0c0c0c'
+  })
+
+  backgroundEffect.resize();
+
+
 // Code for tab section on Early Work
 
 const tabs = document.querySelectorAll('[data-tab-target]')
